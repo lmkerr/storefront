95d41abd1df04baa9924e619b08de9beeb4a0b47
Loren M. Kerr <loren@lorenkerr.com>
2019-12-19 15:05:51 -0700

    added redux-persist to allow state to persist through session

--- presentation/src/redux/store.js
+++ presentation/src/redux/store.js
@@ -1,5 +1,6 @@
 /* Framework */
 import { createStore, applyMiddleware } from 'redux';
+import  { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 /* Internal */
@@ -7,6 +8,8 @@ import rootReducer from './root.reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
