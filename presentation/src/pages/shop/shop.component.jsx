0fa78f730c5563ac3c6d0d7366b4a700f02780e7
Loren M. Kerr <loren@lorenkerr.com>
2019-12-20 08:21:53 -0700

    added shop page redux

--- presentation/src/pages/shop/shop.component.jsx
+++ presentation/src/pages/shop/shop.component.jsx
@@ -1,58 +1,29 @@
 /* Framework */
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 /* Internal */
-//import SHOP_DATA from './shop.data.js';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-
-    this.state = {
-      isLoaded: false,
-      collections: []
-    };
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3000/shop')
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            collections: result,
-          })
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            collection: [],
-            error,
-          })
-        }
-      )
-  }
-
-  render() {
-    const { error, isLoaded, collections } = this.state;
-
-    if(error) {
-      return <div> Error:  {error.message}</div>
-    } else if(!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <div className='shop-page'>
-          {collections.map(({ id, ...otherCollectionsProps }) => (
-            <CollectionPreview key={id} {...otherCollectionsProps} />
-          ))}
-        </div>
-      );
+const ShopPage = ({ collections }) => (
+  
+  <div className='shop-page'>
+    {
+      collections 
+      ? collections.map(({ id, ...otherCollectionsProps }) => (
+        <CollectionPreview key={id} {...otherCollectionsProps} />
+        )) 
+      :
+    <div>Unable to find data...</div>
     }
-  }
-}
+  </div>
 
-export default ShopPage;
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+})
+
+export default connect(mapStateToProps)(ShopPage);
